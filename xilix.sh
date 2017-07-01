66ee771d18c2fde9ef435ad17a5ab102cac47b92
deoxilix <rsen90@gmail.com>
2017-07-01 13:52:16 +0530

    Modified install script, add allSymList

--- xilix.sh
+++ xilix.sh
@@ -4,9 +4,52 @@
 # https://github.com/deoxilix/
 
 CYAN="$(tput setaf 6)"
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+GREEN="$(tput setaf 2)"
 dim="$(tput dim)" # Select dim (half-bright) mode
 rev="$(tput rev)"
+reset="$(tput sgr0)"
 
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
 
 
 printf '%s' "$CYAN"
@@ -15,9 +58,37 @@ printf '%s\n' '/\_\_\_\   /\ \   /\ \       /\ \   /\_\_\_\'
 printf '%s\n' '\/_/\_\/_  \ \ \  \ \ \____  \ \ \  \/_/\_\/_'
 printf '%s\n' '  /\_\/\_\  \ \_\  \ \_____\  \ \_\   /\_\/\_\'
 printf '%s\n' '  \/_/\/_/   \/_/   \/_____/   \/_/   \/_/\/_/'
-printf '%s\n'
+printf '\n'
 printf '%s\n' "${dim}               deoxilix.github.io              "
-printf '%s\n' "${license}"
+echo -n $reset
+
+# if [ -n "$ZSH_VERSION" ]; then
+#   #  cp './xilix.zsh-theme' '~/.oh-my-zsh/themes/'
+#   echo "zsh"
+# else
+#   printf '%s\n' "${RED}   ¯\_(ツ)_/¯"
+#   printf '%s\n' "${reset}where's oh-my-zsh?"
+#
+# fi
+
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+  helixpath="/Users/$USER/.oh-my-zsh/themes/"
+  if echo -en "\n$BGreen---------------------->" && [ -n $(cp './xilix.zsh-theme' ${helixpath}) ]; then
+    printf '\r%s\n' "---------------------------------------------->${reset}"
+    echo -e "helix copied$BGreen ≫$BYellow ${helixpath} ${reset}\n"
+  else
+    echo -e "\r$BRed---------------------------------------------->${reset}\n"
+    echo -e "helix couldnot be copied$BRed ≫$BYellow ${helixpath} ${reset}\n"
+  fi
+elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+  printf '%s\n' "$BRed    ¯\_(ツ)_/¯"
+  printf '%s\n' "${reset}where's oh-my-zsh?"
+else
+  printf '%s\n' "$BRed      ¯\(°_o)/¯"
+  printf '%s\n' "${reset}What shell is this?!"
+fi
+
+# setup() {};
 
 # 0    black
 # 1    red
